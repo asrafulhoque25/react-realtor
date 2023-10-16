90254dbfafb5a0dd04a9d5f36a9cdfdce606afec
Md. Asraful Hoque <asraful15-6625@diu.edu.bd>
2023-10-16 17:53:42 +0600

    link added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
 import './App.css'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import ForgetPassword from './pages/ForgetPassword';
+import Offers from './pages/Offers';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 function App() {
   
 
   return (
     <>
-    <h1>sdfasdf</h1>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/profile" element={<Profile />} /> 
+          <Route path="/forgetpossword" element={<ForgetPassword />} /> 
+          <Route path="/offers" element={<Offers />} /> 
+          <Route path="/signin" element={<Signin />} /> 
+          <Route path="/signup" element={<Signup />} /> 
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
