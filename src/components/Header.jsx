0aaec11b99244641ec6158f72d8e0d731bf3ff7e
asrafulhoque25 <asraful15-6625@diu.edu.bd>
2023-10-20 19:18:32 +0600

    signin add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,31 +11,35 @@ export default function Header() {
   }
 
   return (
-    <div className="container">
-      <div className="navbar bg-white border-b shadow-sm  py-5  sticky top-0 z-50 flex items-center justify-between gap-5">
-        <Link>
+    <div className="bg-white border-b shadow-sm">
+      <div className="container">
+      <div className="navbar py-5  sticky top-0 z-50 flex items-center justify-between gap-5">
+        <Link to={"/"}>
           <img src={logo} className="h-5" alt="" />
         </Link>
         <ul className=" list-none flex items-center gap-6">
           <li>
-            <Link
-              className={`text-sm font-semibold text-gray-400  py-5 border-b-[3px] border-b-transparent 
-              ${pathMatchRoute("/") && "border-b-red-500 text-black" }`} >
+            <Link to={"/"}
+              className={`text-sm font-semibold text-gray-400  py-5 border-b-[3px]  
+              ${pathMatchRoute("/") ? "border-b-red-500 text-black" :"border-b-transparent" }`} >
               Home
             </Link>
           </li>
           <li>
-            <Link className=" text-sm font-semibold text-gray-400  py-5  ">
+            <Link to={"/offers"} className={`text-sm font-semibold text-gray-400  py-5 border-b-[3px]  
+              ${pathMatchRoute("/offers")  ? "border-b-red-500 text-black" :"border-b-transparent" }`}>
               Offers
             </Link>
           </li>
           <li>
-            <Link className=" text-sm font-semibold text-gray-400  py-5  ">
+            <Link to={"/Signin"} className={`text-sm font-semibold text-gray-400  py-5 border-b-[3px]  
+              ${pathMatchRoute("/Signin")  ? "border-b-red-500 text-black" :"border-b-transparent" }`}>
               Sign In
             </Link>
           </li>
         </ul>
       </div>
+    </div>
     </div>
   );
 }
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,38 @@
-
+import { useState } from 'react';
+import login from '/assets/img/login.avif';
 const Signin = () => {
+  const [formData,setFormData]=useState({
+    email:"",
+    password:"",
+  })
+  const{email,password} = formData
+
+  const changeHandle = (e)=>{
+    console.log(e.target.value);
+    setFormData((prev)=>({
+      ...prev,
+      [e.target.id]: e.target.value
+    }))
+  }
   return (
-    <div>Signin</div>
+    <div className="mt-10">
+      <div className="container">
+        <div className="sign-wrap">
+          <h2 className="text-3xl text-center font-bold mb-16">Sign In</h2>
+          <div className="sign-in-wrap grid grid-cols-2 gap-7">
+            <div>
+              <img src={login} className='w-full' alt="" />
+            </div>
+            <div>
+             <form action="">
+              <input type="email" id='email' placeholder='input email' value={email} onChange={changeHandle} />
+              <input type="password" placeholder='input password' value={password} />
+             </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
